d2bdf019899ebe8d6f2a52a3bf04d700fd15009c
Tylwen <elwen.tylwen@hotmail.com>
2025-05-22 23:08:18 +0200

    1.0.4.1

--- src/main/java/fr/tylwen/satyria/dynashop/database/ItemDataManager.java
+++ src/main/java/fr/tylwen/satyria/dynashop/database/ItemDataManager.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
+// import java.util.concurrent.ExecutionException;
 
 import org.bukkit.Bukkit;
 
